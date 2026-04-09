13da663fb2d85a2c1034d30e1d3ba7de620f1700
Nalin Uchicago <nalinprabhath@uchicago.edu>
2026-04-08 21:45:22 -0500

    Update query3.sql

--- part2/Databricks/query3.sql
+++ part2/Databricks/query3.sql
@@ -2,4 +2,4 @@ SET use_cached_result = false;
 SELECT t1.total_amount 
 FROM nyc_taxi_22_25 t1 
 JOIN nyc_taxi_22_25 t2 ON t1.DOLocationID = t2.PULocationID 
-WHERE t1.DOLocationID < 10;
+WHERE t1.DOLocationID IN (1,2,3);
